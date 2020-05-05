e3fc46f17c0d1587df0be586f6709e2b7e373b2b
Vishal Reddy Chintham <chinthamvishal.2019@vitstudent.ac.in>
2020-05-05 14:40:42 +0530

    edited the effects on equipments tab, clients tab and made the number stab horizontal

--- siri.js
+++ siri.js
@@ -47,22 +47,44 @@ var $animate3= $(".laser");
  var $animate4= $(".equipments");
 
  $windows.on("scroll",function(){
- $window_ht= $windows.outerHeight();
+ $window_ht= $windows.height();
  $window_top= $windows.scrollTop();
  $window_bot=($window_top+$window_ht) ;
 
 $.each($animate3,function(){
   $elem=$(this);
-  $elem_ht=$elem.outerHeight(true);
+  $elem_ht=$elem.innerHeight();
   $elem_top=$elem.offset().top;
   $elem_bot=($elem_ht+$elem_top);
 
   if($elem_top<=$window_bot && $elem_bot>=$window_top){
     $elem.addClass("slideleft");
-    console.log("class added");
-    $animate4.delay(4000).fadeIn(1400);
+    $animate4.delay(1500).fadeIn(1400);
+  }
+  if($elem_top>$window_bot || $elem_bot<$window_top){
+    $elem.removeClass("slideleft");
+    $animate4.css("display","none");
   }
 })
+})
 
+var $animate5= $("#cont-5");
+ var $animate6= $(".clientimgs");
 
+ $windows.on("scroll",function(){
+ $window_ht= $windows.height();
+ $window_top= $windows.scrollTop();
+ $window_bot=($window_top+$window_ht) ;
+
+  $elem=$animate5
+  $elem_ht=$elem.height();
+  $elem_top=$elem.offset().top;
+  $elem_bot=($elem_ht+$elem_top);
+
+  if($elem_top<=$window_bot && $elem_bot>=$window_top){
+     $animate6.slideDown(1500);
+  }
+  if($elem_top>$window_bot || $elem_bot<$window_top){
+     $animate6.slideUp(500);
+  }
 })
