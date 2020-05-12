3d50b3f8dc5ec64f9832cdf61b7d69fbbbc07626
vishal-1408 <54095062+vishal-1408@users.noreply.github.com>
2020-05-12 15:20:35 +0530

    Update siri.js

--- siri.js
+++ siri.js
@@ -3,19 +3,19 @@ AOS.init();
 $(".hii").click(function(a){
   $(".lopster").slideToggle(500,function(){
     $("#vi").fadeToggle(300,function(){
-       $("#vis").fadeToggle(300,function(){
-           $("#vish").fadeToggle(300,function(){
-               $("#visha").fadeToggle(300);
+       $("#vis").fadeToggle(400,function(){
+           $("#vish").fadeToggle(500,function(){
+               $("#visha").fadeToggle(600);
            });
        });
     });
     $("#zo").fadeToggle(300,function(){
-       $("#zoe").fadeToggle(300)
+       $("#zoe").fadeToggle(400)
     })
     $("#to").fadeToggle(300,function(){
-       $("#tom").fadeToggle(300,function(){
-          $("#tomm").fadeToggle(300,function(){
-             $("#tommy").fadeToggle(300);
+       $("#tom").fadeToggle(400,function(){
+          $("#tomm").fadeToggle(500,function(){
+             $("#tommy").fadeToggle(600);
            });
        });
     });
@@ -81,8 +81,6 @@ $.each($animate3,function(){
     $animate4.delay(2000).fadeIn(1400);
   }
   if($elem_top>$window_bot || $elem_bot<$window_top){
-    $elem.removeClass("slideleft");
-    $animate4.css("display","none");
   }
 })
 })
@@ -114,23 +112,25 @@ var $animate5= $("#cont-5");
 
  $window.on("scroll",function(){
    $window_top=$window.scrollTop();
-   $window_ht=$window.height();
+   $window_ht=$window.innerHeight();
    $window_bt=($window_ht+$window_top);
 
    $animate7_tp=$animate7.offset().top;
-   $animate7_ht=$animate7.outerHeight();
+   $animate7_ht=$animate7.innerHeight();
    $animate7_bt=($animate7_ht+$animate7_tp);
    if($animate7_bt>=$window_top && $animate7_tp<=$window_bt){
+     // $(".navbar").css("background","#f6eedf");
+     // $(".nav-link,.navbar-brand").css("color","#d63447");
         $animate7.addClass("slideleft");
       $animate8.delay(2300).fadeIn(1400);
-      console.log("fdsfsd");
-
-
    }
 
    if($animate7_bt<$window_top ||  $animate7_tp>$window_bt){
+     // $(".navbar").css("background","#d63447");
+     // $(".nav-link,.navbar-brand").css("color","#f6eedf");
      $animate7.removeClass("slideleft");
      $animate8.css("display","none");
+
    }
 
  })
@@ -138,3 +138,23 @@ var $animate5= $("#cont-5");
  $("input[type=\"email\"]").click(function(){
    $(".tar").slideDown(1000);
  })
+ //
+ // var $animate9= $("#cont-2");
+ // var $windows=$(window);
+ //
+ //  $windows.on("scroll",function(){
+ //  $window_ht= $windows.outerHeight();
+ //  $window_top= $windows.scrollTop();
+ //  $window_bot=($window_top+$window_ht) ;
+ //
+ //  $animate9_tp=$animate9.offset().top;
+ //  $animate9_ht=$animate9.innerHeight();
+ //  $animate9_bt=($animate9_ht+$animate9_tp);
+ //  if($animate9_bt>=$window_top && $animate9_tp<=$window_bt){
+ //     console.log("sdffsdfsdfdsf");
+ //     $(".navbar").css("background","#f6eedf");
+ //     $(".nav-link,.navbar-brand").css("color","#d63447");
+ //   }
+
+ // })
+
